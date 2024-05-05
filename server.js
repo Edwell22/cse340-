f94018b7723225edf947d472ccd6fadb4d11e954
Edwell22 <edwellkakunguwo358@gmail.com>
2024-05-05 08:37:10 +0200

    new

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -13,6 +14,20 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+app.use(require("./routes/static"))
+//index routes
+app.get("/",function(req, res){
+  res.render("index", {title: "Home"})})
+
+// View Engine and Templates
+
+
+
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+
 app.use(static)
 
 /* ***********************
